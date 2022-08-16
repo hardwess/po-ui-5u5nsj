416760b770ec9ff6c30ec06e6c8d2398c5d72808
hardwess <hardwess@gmail.com>
2022-08-16 13:43:39 -0300

    1 file modified

--- src/app/Views/login/login.component.ts
+++ src/app/Views/login/login.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { PoPageLoginModule,PoPageLoginComponent } from '@po-ui/ng-templates';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
